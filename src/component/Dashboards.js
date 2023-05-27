cf1e3fa0fd35fed725fac554595d97e00739fb68
Oluwadebo <ogunweoluwadebo@gmail.com>
2023-05-27 20:06:15 +0100

    Server 5020 forntend Dasboards started

---
+++ src/component/Dashboards.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Dashboards = () => {
+    // const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    //     target: '#navbar-example'
+    // })
+    return (
+        <>
+            <div className="">
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-2">
+                            <p>Home</p>
+                        </div>
+                        <div className="col-10">
+                            <div className="scrollSpy">
+                                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatum enim laudantium reprehenderit veniam voluptate ipsum delectus error? Neque esse sapiente iure dignissimos! Ex molestiae non deleniti, sapiente quo earum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique numquam iure optio delectus ea saepe culpa sequi quisquam iusto molestias commodi illum expedita tempore esse, cumque vel omnis placeat! Dolore.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatum enim laudantium reprehenderit veniam voluptate ipsum delectus error? Neque esse sapiente iure dignissimos! Ex molestiae non deleniti, sapiente quo earum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique numquam iure optio delectus ea saepe culpa sequi quisquam iusto molestias commodi illum expedita tempore esse, cumque vel omnis placeat! Dolore.Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui voluptatum enim laudantium reprehenderit veniam voluptate ipsum delectus error? Neque esse sapiente iure dignissimos! Ex molestiae non deleniti, sapiente quo earum? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique numquam iure optio delectus ea saepe culpa sequi quisquam iusto molestias commodi illum expedita tempore esse, cumque vel omnis placeat! Dolore.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Dashboards
